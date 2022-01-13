1a8b189004ad42e06c6b207a27ffbc0c34ba7c7d
FernandesW <wilfredfernandes562@gmail.com>
2022-01-13 10:43:42 +0530

    Temp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,43 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+ 
 describe('AppComponent', () => {
+  let component:AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        AppComponent
+      ]
     }).compileComponents();
   }));
 
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    
+  });
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+    console.log("1st Test Case Check");
+    console.log(fixture.debugElement.nativeElement);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-jest'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have as title 'angular-karma'`, () => {
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-jest');
+    expect(app.title).toEqual('angular-karma');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-jest app is running!');
-  });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HeroService } from './hero.service'
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +14,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
